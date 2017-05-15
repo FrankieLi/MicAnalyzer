86e57da6ed99cd8a877c544e6cdd6cdc68fd8e90
Frankie Li <shiu.fai.li@gmail.com>
2017-05-14 23:07:20 -0700

    Got reading and vertex generation to work.

---
+++ Python/xdm_toolkit/xdm_toolkit/xdm_assert.py
@@ -0,0 +1,8 @@
+__copyright__ = '2017 Frankie Li. All Rights Reserved.'
+__author__ = 'Frankie Li'
+
+""" Asserts and debugs.
+"""
+
+def runtime_assert(cond, err_msg):
+	assert cond, err_msg
---
+++ Python/xdm_toolkit/xdm_toolkit/xdm_plot.py
@@ -0,0 +1,14 @@
+__copyright__ = '2017 Frankie Li. All Rights Reserved.'
+__author__ = 'Frankie Li'
+
+""" XDM visualiation library.
+"""
+
+def plot_mic(snp, sidewidth, plot_opt, confidence):
+	"""
+		Main plot driver.
+	"""
+
+	raise
+
+
